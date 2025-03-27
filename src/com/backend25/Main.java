a06031a4800dc4af16c7d0a210e5a327ec87efc8
Reynaldi119 <80832429+Reynaldi119@users.noreply.github.com>
2025-03-27 19:14:35 +0700

    first commit

---
+++ src/com/backend25/Main.java
@@ -0,0 +1,24 @@
+package com.backend25;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Juara Coding");
+        int x = 10;
+        // camelCase
+        // snake_case
+        // SNAKE_CASE
+        //PascalCase
+        //kebab-case
+
+        // scope if
+        if (true) {
+            System.out.println("Statement if");
+            System.out.println(x);
+        }
+
+        for (int i = 0; i<5; i++) {
+            System.out.println(i);
+            System.out.println(x);
+        }
+    }
+}
